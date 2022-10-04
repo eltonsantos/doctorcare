3531b68506e10a7690faa2d805861c4c6b1610f5
Elton Santos <eltonaxl@hotmail.com>
2022-10-03 23:34:14 -0300

    finish project

--- main.js
+++ main.js
@@ -79,23 +79,23 @@ ScrollReveal({
 
 
 // Animar números
-const counters = document.querySelectorAll('.animate-number');
-const speed = 200;
+// const counters = document.querySelectorAll('.animate-number');
+// const speed = 200;
 
-counters.forEach( counter => {
-   const animate = () => {
-      const value = +counter.getAttribute('number');
-      const data = +counter.innerText;
+// counters.forEach( counter => {
+//    const animate = () => {
+//       const value = +counter.getAttribute('number');
+//       const data = +counter.innerText;
      
-      const time = value / speed;
-     if(data < value) {
-          counter.innerText = Math.ceil(data + time);
-          setTimeout(animate, 4);
-        }else{
-          counter.innerText = value;
-        }
+//       const time = value / speed;
+//      if(data < value) {
+//           counter.innerText = Math.ceil(data + time);
+//           setTimeout(animate, 4);
+//         }else{
+//           counter.innerText = value;
+//         }
      
-   }
+//    }
    
-   animate();
-});
+//    animate();
+// });
